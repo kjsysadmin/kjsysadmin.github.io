935f21defb107b9be6172e52142145fbfb938144
kjsysadmin <kjhnstn23@gmail.com>
2016-11-27 03:55:40 -0600

    sadf

---
+++ firebaseJS.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyDWcbrLDMBSpIWSTDcCoKbhrVybupm_z1A",
+  authDomain: "kjsysadmin.firebaseapp.com",
+  databaseURL: "https://kjsysadmin.firebaseio.com",
+  storageBucket: "kjsysadmin.appspot.com",
+  messagingSenderId: "393888601999"
+};
+firebase.initializeApp(config);
