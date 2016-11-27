f9d746ff652d6d22eeecf9df07b2206e947a9762
kjsysadmin <kjhnstn23@gmail.com>
2016-11-27 03:55:56 -0600

    removed firebasejs

--- firebaseJS.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: "AIzaSyDWcbrLDMBSpIWSTDcCoKbhrVybupm_z1A",
-  authDomain: "kjsysadmin.firebaseapp.com",
-  databaseURL: "https://kjsysadmin.firebaseio.com",
-  storageBucket: "kjsysadmin.appspot.com",
-  messagingSenderId: "393888601999"
-};
-firebase.initializeApp(config);
